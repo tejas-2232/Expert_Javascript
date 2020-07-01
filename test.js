1b0e86a14c7ca2a2a196e3af94a5139b8f67594f
Tejas <tejasbachhav98@gmail.com>
2020-07-01 23:50:54 +0530

    if-else explained in detail

--- test.js
+++ test.js
@@ -44,7 +44,28 @@ else if(age<10 && age>3)
 {
     document.write("You also go to school");
 }
-else
+else if(age>20 && age<=30)
 {
-    document.write("do whatever you want");   
+    document.write("Focus on career");   
+}
+else if(age>30 && age<=40)
+{
+    document.write("Focus on Financial status");
+}
+else if(age>40 && age<=50)
+{
+    document.write("Focus on health");
+}
+else if(age>50 && age<=70)
+{
+    document.write("Focus on Your mind");
+}
+else if(age>70 && age<=100){
+    document.write("Nurture your grand childrens");
+}
+else if(age>100 && age<=200)
+{document.write("You are entering in new mode of life");}
+
+else{
+    document.write("Have you taken weed?? sillyman");
 }
