72f48ac00da6ec063c51def7c81b02b629e52018
Tejas <tejasbachhav98@gmail.com>
2020-07-20 00:46:04 +0530

    created & appended second element in JS

--- Elements/tut16.js
+++ Elements/tut16.js
@@ -1,22 +1,35 @@
 console.log("This is tutorial about- creating, Removing and replacing elements in Javascript ");
 
 let element = document.createElement("li")
-
 // add class name to li element
 
-element.className='childul'; // by this line you will get <li> element of class chilul very simple
-element.id=" createdLi";     // same like class,ID is also added in <li> element
-element.setAttribute('title',"research paper") 
+element.className = 'childul'; // by this line you will get <li> element of class chilul very simple
+element.id = " createdLi";     // same like class,ID is also added in <li> element
+element.setAttribute('title', "research paper")
 //console.log(element); // you will get empty <li> element
 
 // now we will append element created in UL. before that add inner text in 'element' 
-element.innerText="Hello people,You all are beautiful";
+element.innerText = "Hello people,You all are beautiful";
 
-
-let ul= document.querySelector('ul.this');
+let ul = document.querySelector('ul.this');
 ul.appendChild(element); // adding element to <UL>
 
 console.log(ul);   // querySelector gives the first element if UL
-
 console.log(element);
+console.log("=========================================================");
+
+console.log("creating second element in DOM")
+
+let element2 = document.createElement('li')
+element2.className = 'getul'
+element2.id = "sub1"
+element2.innerHTML = '<b>blockchain</b>'
+
+// select li to add elemet in it using quey selector
+
+let dd = document.querySelector('ul.subjects')
+dd.appendChild(element2);
+console.log(dd);
+console.log(element2);
+
 
