44acc6058250771ff07797bce7eb0140e31239df
Tejas <tejasbachhav98@gmail.com>
2020-07-20 00:08:52 +0530

    added <li> element

--- Elements/tut16.js
+++ Elements/tut16.js
@@ -1,2 +1,5 @@
 console.log("This is tutorial about- creating, Removing and replacing elements in Javascript ");
 
+let element = document.createElement("li")
+
+console.log(element); // you will get empty <li> element
