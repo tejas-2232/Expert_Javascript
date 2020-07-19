a1b731a521d8a62db9e08f0f5a462fa28db2b61c
Tejas <tejasbachhav98@gmail.com>
2020-07-20 00:13:43 +0530

    created element using createelement function and added title,ID to the element

--- Elements/tut16.js
+++ Elements/tut16.js
@@ -2,4 +2,9 @@ console.log("This is tutorial about- creating, Removing and replacing elements i
 
 let element = document.createElement("li")
 
+// add class name to li element
+
+element.className='childul'; // by this line you will get <li> element of class chilul very simple
+element.id=" createdLi";     // same like class,ID is also added in <li> element
+element.setAttribute('title',"research paper") 
 console.log(element); // you will get empty <li> element
