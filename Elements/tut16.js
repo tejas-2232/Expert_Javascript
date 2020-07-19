b23bae6df85ddddf02a22ba295c95a7f0d3a626c
Tejas <tejasbachhav98@gmail.com>
2020-07-20 00:23:37 +0530

    appended created element in DOM

--- Elements/tut16.js
+++ Elements/tut16.js
@@ -7,4 +7,16 @@ let element = document.createElement("li")
 element.className='childul'; // by this line you will get <li> element of class chilul very simple
 element.id=" createdLi";     // same like class,ID is also added in <li> element
 element.setAttribute('title',"research paper") 
-console.log(element); // you will get empty <li> element
+//console.log(element); // you will get empty <li> element
+
+// now we will append element created in UL. before that add inner text in 'element' 
+element.innerText="Hello people,You all are beautiful";
+
+
+let ul= document.querySelector('ul.this');
+ul.appendChild(element); // adding element to <UL>
+
+console.log(ul);   // querySelector gives the first element if UL
+
+console.log(element);
+
