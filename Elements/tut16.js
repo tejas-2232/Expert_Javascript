cb96a77c93ed93113d3aefff99c7a73597153ed9
Tejas <tejasbachhav98@gmail.com>
2020-07-21 23:22:14 +0530

    learned replacing elements in javascript

--- Elements/tut16.js
+++ Elements/tut16.js
@@ -3,10 +3,16 @@ console.log("This is tutorial about- creating, Removing and replacing elements i
 let element = document.createElement("li")
 // add class name to li element
 
+let text= document.createTextNode(" i am a text node");
+element.appendChild(text);
+
+
+
 element.className = 'childul'; // by this line you will get <li> element of class chilul very simple
 element.id = " createdLi";     // same like class,ID is also added in <li> element
 element.setAttribute('title', "research paper")
 //console.log(element); // you will get empty <li> element
+
 
 // now we will append element created in UL. before that add inner text in 'element' 
 element.innerText = "Hello people,You all are beautiful";
@@ -29,7 +35,18 @@ element2.innerHTML = '<b>blockchain</b>'
 
 let dd = document.querySelector('ul.subjects')
 dd.appendChild(element2);
-console.log(dd);
-console.log(element2);
+// console.log(dd);
+// console.log(element2);
 
 
+//--------------------------------------------
+// Replacing element
+let elem2=document.createElement('h3');
+
+elem2.id='elem2';
+elem2.className='elem2';
+let tnode=document.createTextNode("this is created node for elem2");
+elem2.appendChild(tnode);
+
+
+element.replaceWith(elem2);
