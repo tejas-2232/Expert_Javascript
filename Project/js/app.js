a97eb5b1ca8595974ce9226742831a4a0b72af84
Tejas <tejasbachhav98@gmail.com>
2020-07-24 22:29:54 +0530

    added eventlister to add button

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,1 +1,17 @@
 console.log('welcome to Magic notes')
+// add event listner to add note button
+let addBtn=document.getElementById('addBtn');   
+addBtn.addEventListener("click",function(e){
+
+    let addTxt = document.getElementById('addTxt'); 
+    let notes = localStorage.getItem("notes");  // to check if any note already exists in localstorage or not
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes); // if any string is found then pass it//notesobj can be an array
+    }
+    notes.push(addTxt.value);  // adding text written in note to notes variable
+    localStorage.setItem("notes",JSON.stringify(notes));   // converting notes array to string, cause in localstorage we can set it only in string 
+    addTxt.value = "";                                    // settign value again to null
+})
