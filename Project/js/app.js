6d4d804ef4fa1c73339cf24d4310690c191107d7
Tejas <tejasbachhav98@gmail.com>
2020-07-26 01:16:41 +0530

    fixed the typos and adding notes to arrray is working fine

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,20 +1,17 @@
 console.log('welcome to Magic notes')
 // add event listner to add note button
 let addBtn = document.getElementById('addBtn');
-
 addBtn.addEventListener('click',function(e){
     let addTxt = document.getElementById("addTxt");
-    let notes = localStorage.getItem("notes");
-    
-    if(notes==null){
-        note=[];
+    let notes = localStorage.getItem("notes");   
+    if(notes == null){
+        notesObj = [];
     }
     else{
-        note=JSON.parse(notes);
+        notesObj = JSON.parse(notes);
     }
-
-    notesObj.push(addTxt.nodeValue);
-    localStorage.setItem("notes",JSON.stringify(note));
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes",JSON.stringify(notesObj));
     addTxt = "";
-    console.log(note);
+    console.log(notesObj);
 })
