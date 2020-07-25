171abbd169966dd331a5db4dd4d63befdea73621
Tejas <tejasbachhav98@gmail.com>
2020-07-25 23:59:11 +0530

    variale channged

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,17 +1,20 @@
 console.log('welcome to Magic notes')
 // add event listner to add note button
-let addBtn=document.getElementById('addBtn');   
-addBtn.addEventListener("click",function(e){
+let addBtn = document.getElementById('addBtn');
 
-    let addTxt = document.getElementById('addTxt'); 
-    let notes = localStorage.getItem("notes");  // to check if any note already exists in localstorage or not
-    if(notes == null){
-        notesObj = [];
+addBtn.addEventListener('click',function(e){
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    
+    if(notes==null){
+        note=[];
     }
     else{
-        notesObj = JSON.parse(notes); // if any string is found then pass it//notesobj can be an array
+        note=JSON.parse(notes);
     }
-    notes.push(addTxt.value);  // adding text written in note to notes variable
-    localStorage.setItem("notes",JSON.stringify(notes));   // converting notes array to string, cause in localstorage we can set it only in string 
-    addTxt.value = "";                                    // settign value again to null
+
+    notesObj.push(addTxt.nodeValue);
+    localStorage.setItem("notes",JSON.stringify(note));
+    addTxt = "";
+    console.log(note);
 })
