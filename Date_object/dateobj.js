01168661fd6c2fd2dd4584632c5f718605d9dfb4
Tejas <tejasbachhav98@gmail.com>
2020-08-03 23:14:32 +0530

    learned about date methods in javascript

--- Date_object/dateobj.js
+++ Date_object/dateobj.js
@@ -1,4 +1,46 @@
 console.log("welcome to Date object tutorial")
 
-let todat = new Date();
-console.log(todat);
+let today = new Date();
+// console.log(today); // its data type is reference
+
+let odate= new Date('June 13 1976');
+
+odate= new Date('16/04/1888');  // dd/mm/yyyy - invalid
+odate = new Date('04/16/1990')
+// console.log(odate);
+
+let a;
+a= odate.getDay();  // (04/16/1990) is monday. Here calender starts from sunday. And monday is one next to sunday. so sunday=0 and monday=1 and tuesday=2
+console.log("getDay",a);
+
+let b;
+b=odate.getDate();
+console.log('getDate',b);
+
+let c;
+c=odate.getMinutes();
+console.log('getMinutes',c);
+
+let d;
+d=odate.getSeconds();
+console.log('getSeconds',d);
+
+let e;
+e=odate.getHours();
+console.log('getHours',e);
+
+
+f=odate.getTime();
+console.log('numbser of seconds since 1JAN1970',f);
+
+/* methods to try 
+odate.getMilliseconds()
+odate.getMonth()
+odate.setDate()
+odate.setMonth()
+odate.setFullYear()
+odate.setMinutes()
+odate.setHours()
+odate.setSeconds()
+
+*/
