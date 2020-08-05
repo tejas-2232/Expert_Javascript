713fb2b9abca26202a2a192ceca00c3898a898b1
Tejas <tejasbachhav98@gmail.com>
2020-08-05 23:18:56 +0530

    exercise of editable DIV

---
+++ Editable_div/tut25.js
@@ -0,0 +1,8 @@
+console.log('This is tutorial on editable div')
+
+/* you have to create div & inject it into the page which contains heading.whenever someone
+clicks on the div,i should be converted into the editable item. whenever user clicks away (blur). 
+save the into local storage
+*/
+
+let divElem=document.createElement('div');
