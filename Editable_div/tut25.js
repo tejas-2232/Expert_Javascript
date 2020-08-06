1c684424895f18f2008d5df521250068598abf24
Tejas <tejasbachhav98@gmail.com>
2020-08-06 23:44:37 +0530

    added event listners

--- Editable_div/tut25.js
+++ Editable_div/tut25.js
@@ -5,4 +5,53 @@ clicks on the div,i should be converted into the editable item. whenever user cl
 save the into local storage
 */
 
-let divElem=document.createElement('div');
+let divElem = document.createElement('div');
+
+// add text to that created element
+
+let text = document.createTextNode('This is my element. CLick to edit it');
+divElem.appendChild(text);
+
+// give id,style,class
+
+divElem.setAttribute('id', 'elem');
+divElem.setAttribute('class', 'elem');
+divElem.setAttribute('style', 'border:2px solid black; width:154 pz; margin:34px; padding:23px;');
+
+//grab the main container
+
+let container = document.querySelector('.container');
+
+let first = document.getElementById('myfirst');
+
+
+// insert elem before id first
+
+container.insertBefore(divElem, first);
+
+console.log(divElem, container, first)
+
+// add event listenr to divElem
+
+divElem.addEventListener('click',function(){
+    let notextareas = document.getElementsByClassName('textarea').length;
+    if(notextareas == 0){
+
+    let html= elem.innerHTML;
+    divElem.innerHTML = ` <textarea class=" textarea form-control" class='textarea' id="textarea" rows="3">${html} </textarea>` 
+    }
+    // listen for blur event on textarea
+    
+    let textarea = document.getElementById('textarea');
+
+    textarea.addEventListener('blur',function(){
+
+        elem.innerHTML=  textarea.value;
+    })    
+
+
+
+})
+
+
+ let textarea = document.getElementById('textarea');
