7244555b8e74fddb5d476d07a5706df2a472c972
Tejas <tejasbachhav98@gmail.com>
2020-08-09 23:31:49 +0530

    started with object literals and constructors in javascipt

---
+++ object-literals_and_constructors/tut27.js
@@ -0,0 +1,12 @@
+console.log('this is tutorial 27')
+
+//object literal for creating objects
+let car={
+    name: 'honda civic',
+    topSpeed:300,
+    run:function run() {
+        console.log('car is running');
+
+    }
+}
+console.log(car)
