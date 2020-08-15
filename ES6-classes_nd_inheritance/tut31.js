de26bcad702c2d59b85721d611fefecd76d8ceed
Tejas <tejasbachhav98@gmail.com>
2020-08-15 21:50:39 +0530

    lesson completed

--- ES6-classes_nd_inheritance/tut31.js
+++ ES6-classes_nd_inheritance/tut31.js
@@ -14,12 +14,46 @@ class Employee{
     joiningYear(){
         return 2019 - this.experience //2020-14
     }
+
+    static add(a,b){
+        return a+b;
+    }
 }
 
 rohn = new Employee('rohn',14,'division');
-console.log(rohn);
+// console.log(rohn);
+
+class programmer extends Employee{
+    constructor(givenName,givenExperience,givenDivision,Language,github){
+
+        super(givenName,givenExperience,givenDivision);
+
+        this.Language=Language;
+        this.github-github;
+    
+    }
+
+    favoriteLanguage(){
+        if(this.Language=='python'){
+            return `python`;
+        }
+        else{
+            return `javascript`;
+        }
+    }
+
+    static multiply(a,b){
+        return a*b;
+    }
+
+}
 
 
+rohan = new programmer("rohan",5,'first',"dart","rohan785");
+console.log(rohan);
+console.log(rohan.favoriteLanguage());
+
+console.log(programmer.multiply(5,8));
 
 
 
