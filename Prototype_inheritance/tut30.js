eb90a6ec6007e818e095d8fa6b1efada33d4781a
Tejas <tejasbachhav98@gmail.com>
2020-08-15 10:44:35 +0530

    completed the assignment with solution

--- Prototype_inheritance/tut30.js
+++ Prototype_inheritance/tut30.js
@@ -13,10 +13,56 @@ const proto={
 
 }
 
-const tj = Object.create(proto)
 
+//This creates tj object
+
+ 
+const tj = Object.create(proto)
 tj.name= 'Tommy'
 tj.role = "designer"
 
 tj.changeName('Rohn')  // name will update
-console.log(tj);
+// console.log(tj); 
+ 
+
+//This also creates tj object
+const tj1 = Object.create(proto, {
+    name:{value:'tommy',writable:true},
+    role:{value:'designer'}
+
+})
+tj1.changeName("tommmy2")
+// console.log(tj1)
+
+// employee contstructor
+
+function Employee(name,salary,experience){
+    this.name= name;
+    this.salary= salary;
+    this.experience= experience;
+
+}
+
+// slogan
+Employee.prototype.slogan = function(){
+    return `It's Never too late. Regards ${this.name}`;
+}
+
+// create object from this constructor 
+let tjobj = new Employee("Snoden",500000,33);
+// console.log(tjobj.slogan())
+
+// programmer contructor
+
+function programmer(name,salary,experience,language){
+    Employee.call(this,name,salary,experience);
+    this.language=language;
+}
+// Inherit prototype
+programmer.prototype= Object.create(Employee.prototype);
+
+// manually setting the prototype
+programmer.prototype.constructor = programmer;
+
+let danny = new programmer('rohan',20000,4,'python');
+console.log(danny)
