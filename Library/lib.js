5090ed064914032d09e926e3f49f15c58daf6f1d
Tejas <tejasbachhav98@gmail.com>
2020-08-19 23:46:25 +0530

    we can add books in a store using the form

--- Library/lib.js
+++ Library/lib.js
@@ -1,7 +1,9 @@
+console.log('welcome to Library');
+
 // constructor
 
 function Book(name, author, type) {
-    this.name = title;
+    this.name = name;
     this.author = author;
     this.type = type;
 }
@@ -19,9 +21,36 @@ function Display() {
 let libraryForm = document.getElementById('libraryForm');
 libraryForm.addEventListener('submit', libraryFormSubmit);
 
-function libraryFormSubmit(){
+function libraryFormSubmit(e){
     console.log('You have successfully submitted library form');
-}
+    let name=document.getElementById('bookname').value;
+    let author=document.getElementById('author').value;
+    
 
+    let Fantasy= document.getElementById('Fantasy');
+    let Biography=document.getElementById('Biography');
+    let ComputerScience=document.getElementById('ComputerScience');
+
+    let type;
+
+    if(Fantasy.checked){
+        type=Fantasy.value;
+    }
+    else if (Biography.checked){
+        type=Biography.value;
+    }
+    else if (ComputerScience.checked) {
+        type=ComputerScience.value;
+    }
+
+    let sd = new Book(name, author,type);
+    console.log(sd);
+    e.preventDefault();
+
+
+
+
+}
+ 
 
     
