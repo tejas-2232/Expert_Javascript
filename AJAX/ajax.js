cbce616b172589586c05fd562e1409fd967b6d91
Tejas <tejasbachhav98@gmail.com>
2020-08-28 00:00:41 +0530

    xhr added

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -3,3 +3,6 @@ console.log("AJAX tutorial in one GO")
 let fetchBtn = document.getElementById('fetchBtn');
 fetchBtn.addEventListener('click',buttonClickHandler);
 
+function buttonClickHandler(){
+    const xhr = new XMLHttpRequest()
+}
