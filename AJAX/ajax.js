f6b7e91afc74ede0fd66c94d29d28d62261eb3d9
Tejas <tejasbachhav98@gmail.com>
2020-08-28 22:47:15 +0530

    fetching the data through text file~

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -4,5 +4,36 @@ let fetchBtn = document.getElementById('fetchBtn');
 fetchBtn.addEventListener('click',buttonClickHandler);
 
 function buttonClickHandler(){
-    const xhr = new XMLHttpRequest()
+
+    console.log("you have clicked the fetchBtn");
+    
+    //Instantiate the xhr object
+    const xhr = new XMLHttpRequest();
+
+    //open the object
+    xhr.open('GET','filedata.txt',true);
+
+    //what to do on progress(optional)
+    xhr.onprogress=function(){
+        if(this.status === 200){
+            console.log('On progress');
+        }
+        else{
+            console.error('Some error has occured');
+        }  
+    }
+    //what to do when response is ready
+    xhr.onload = function(){
+        console.log(this.responseText)
+    }
+
+    // send the request
+    xhr.send();
+    console.log("we are done");
+
+
+
 }
+
+
+
