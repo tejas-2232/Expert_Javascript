4f09a73d2a2a0c7ab47433e74921bfd0e6efa710
Tejas <tejasbachhav98@gmail.com>
2020-09-01 00:06:38 +0530

    function definition has been added

--- callback_functions/tut37.js
+++ callback_functions/tut37.js
@@ -10,7 +10,32 @@ const students = [
 
 // create two functions
 
-// function enrollStundent()
+function enrollStudent(student){
+    setTimeout(function() {
+        students.push(student);
+        console.log('student has been enrolled');       
+    }, 3000);
+}
 
-// function getStudents()
+// settime out is used because server will take time to response and react 
+// that's why we set timeout of 3 seconds
 
+
+function getStudents(){
+    setTimeout(function() {
+        let str = "";
+
+        students.forEach(function(student){
+            str += `<li> ${student.name} </li> `
+        });
+        document.getElementById('students').innerHTML = str;
+        console.log('student data has been fetched');       
+
+    }, 1000);
+}
+
+
+let newstudent = {name:"john", subject: "Machine algo"};
+
+enrollStudent(newstudent);
+getStudents();
