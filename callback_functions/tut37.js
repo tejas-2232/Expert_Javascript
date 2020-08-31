8aab5d2adedb49a54cdbfdaaf2b7240bec7820ba
Tejas <tejasbachhav98@gmail.com>
2020-08-31 23:49:20 +0530

    callback functions inintialised

---
+++ callback_functions/tut37.js
@@ -0,0 +1,16 @@
+console.log("welcome to tutorial on callback functions")
+
+
+// pretend that response to this is coming from server
+const students = [
+    {name:"sam",subject : "Javascipt"},
+    {name:"Ron",subject : "Ruby"}
+
+]
+
+// create two functions
+
+// function enrollStundent()
+
+// function getStudents()
+
