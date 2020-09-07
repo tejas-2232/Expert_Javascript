cec278cffe787c72a729011ad401a97d97c821e5
Tejas <tejasbachhav98@gmail.com>
2020-09-07 22:34:16 +0530

    started with Regular expressions

---
+++ Regular_expressions/regex.js
@@ -0,0 +1,11 @@
+console.log("Regular expresseion part 1")
+
+function validate(){
+    // regular expression are treated as object in javascript
+    
+    var username = document.getElementById("uname");
+    var regx = /E00/;
+
+    
+
+}
