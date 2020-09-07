a8a916a75f3a06e9551caff52fa9ceddd9edd2bc
Tejas <tejasbachhav98@gmail.com>
2020-09-07 22:46:43 +0530

    code updated

--- Regular_expressions/regex.js
+++ Regular_expressions/regex.js
@@ -3,9 +3,19 @@ console.log("Regular expresseion part 1")
 function validate(){
     // regular expression are treated as object in javascript
     
-    var username = document.getElementById("uname");
+    var username = document.getElementById("uname").value;
     var regx = /E00/;
 
-    
 
+    //test = Returns a Boolean value that indicates whether or not a pattern exists in a searched string.
+    if(regx.test(username))
+    {
+        console.log('username valid');
+    }
+    else{
+        console.log('Invalid username');
+        alert("invalid");
+        document.getElementById('user').style.visibility="visible";
+
+    }
 }
