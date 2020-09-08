c5bdcb81efa1925db2dcbae2153019c08b5775ae
Tejas <tejasbachhav98@gmail.com>
2020-09-08 22:16:49 +0530

    main driving code added in JS file

--- Regular_expressions/MobileNo_validation/regx.js
+++ Regular_expressions/MobileNo_validation/regx.js
@@ -4,20 +4,24 @@ console.log("Mobile number validation")
 function validate(){
     // regular expression are treated as object in javascript
     
-    var username = document.getElementById("").value;
-    var regx = //;
+    var text = document.getElementById("text1").value;
+    var regx = /[7-9][0-9]{9}/;
     
 
     //test = Returns a Boolean value that indicates whether or not a pattern exists in a searched string.
-    if(regx.test())
+    if(regx.test(regx))
     {
         console.log('Number valid');
-        document.getElementById().style
+        document.getElementById('text22').innerHTML="Valid Number";
+        document.getElementById('text22').style.visibility="visible";
+        document.getElementById('text22').style.color="green";
     }
     else{
         console.log('Invalid Number');
         alert("invalid");
-        document.getElementById('user').style.visibility="visible";
+        document.getElementById('text22').innerHTML="Invalid ";
+        document.getElementById('text22').style.visibility = "visible";
+        document.getElementById('text22').style.color="red";
 
     }
 }
