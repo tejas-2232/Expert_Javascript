c315127d0044c155ac3b902bc8b579de42716c52
Tejas <tejasbachhav98@gmail.com>
2020-09-09 22:28:35 +0530

    testing phase of email validation

--- Regular_expressions/Email_validation/mail.js
+++ Regular_expressions/Email_validation/mail.js
@@ -1,26 +1,32 @@
 console.log("Regular expressions")
 console.log("Email-ID validation")
 
+/*        \d - match any digit..[0-9]
+          \w  - match any word character (a-z,A-Z,0-9 &_)
+ */
 function validate(){
     // regular expression are treated as object in javascript 
     var email = document.getElementById("em1").value;
-    var regx = /$/;
+
+    // var regx = /\w[@][a-z][.]^[com|co][.]^[in]/;
+     var regx= /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9\-]+).([a-z]{2,10}).([a-z]{2,10})$/
     
 
     //test = Returns a Boolean value that indicates whether or not a pattern exists in a searched string.
     if(regx.test(email))
     {
         console.log('Email Valid');
-        document.getElementById('').innerHTML="Valid ";
-        document.getElementById('').style.visibility="visible";
-        document.getElementById('').style.color="green";
+        alert("valid");
+        document.getElementById('em22').innerHTML="Valid ";
+        document.getElementById('em22').style.visibility="visible";
+        document.getElementById('em22').style.color="green";
     }
     else{
         console.log('Email Invalid');
         alert("invalid");
-        document.getElementById('text22').innerHTML="Invalid ";
-        document.getElementById('text22').style.visibility = "visible";
-        document.getElementById('text22').style.color="red";
+        document.getElementById('em22').style.visibility = "visible";
+        document.getElementById('em22').innerHTML="Invalid ";
+        document.getElementById('em22').style.color="red";
 
     }
 }
